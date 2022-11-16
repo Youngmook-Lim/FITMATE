67266a9724626713eba87069352392848ec95d2f
Youngmook Lim <youngmookk@gmail.com>
2022-11-17 01:34:53 +0900

    Move JWT parse functionality to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,15 +15,37 @@ export default {
   components: {
     TheHeaderNav,
   },
+  methods: {
+    parseJWT(token) {
+      var base64Url = token.split(".")[1];
+      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      var jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+          .join("")
+      );
+      return JSON.parse(jsonPayload);
+    },
+  },
   created() {
-    if (!sessionStorage.getItem("access-token")) {
-      this.$router.push({ name: "LoginView" });
-    } else {
-      axios.get(`userApi/detail`, this.u_id).then((res) => {
-        this.$store.commit("SET_MY_USER", res.data);
-      });
-    }
     // 등록되어 있으면 axios로 지금 로그인 한 계정 갖고와서 myUser 업데이트 시켜주기
+
+    // sessionStorage.setItem(
+    //   "access-token",
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJteU1lc3NhZ2UiOiJXVEZGRkZGIEJST09PT08ifQ.Tf3M9Y8v6vzhDENFgucVt4c9KN1GQn0avqBEpMR_FRQ"
+    // );
+    const token = sessionStorage.getItem("access-token");
+    if (token) {
+      const payload = this.parseJWT(token);
+      console.log(payload);
+      axios
+        .get(`userApi/detail`, payload.u_id)
+        .then((res) => this.$store.commit("SET_MY_USER", res.data));
+    } else {
+      this.$router.push({ name: "LoginView" });
+    }
   },
 };
 </script>
